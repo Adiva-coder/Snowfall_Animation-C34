f4dc2b4f1f068440e5a09b96a7354d1a01513b6b
Adiva-coder <76836516+Adiva-coder@users.noreply.github.com>
2021-04-24 10:06:24 +0530

    Update snow.js

--- snow.js
+++ snow.js
@@ -1,19 +1,41 @@
 class Snow{
-    constructor(x, y){
-        var options={
-            isStatic: false
-        };
-        this.image = loadImage("snow4.webp");
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: false,
+            restitution :0.4,
+            friction :0.0
+        }
         this.body = Bodies.rectangle(x, y, 50, 50, options);
         this.width = 50;
         this.height = 50;
+        this.image = loadImage("snow4.webp");
         World.add(world, this.body);
-    }
-    display(){
-        var pos = this.body.position;
+        this.Visibility = 255;
+      }
+      display(){
+        
+        if(this.body.position.y > 730){
+            var pos= this.body.position;
+            World.remove(world, this.body);
+            push();
+            tint(255, this.Visibility);
+            this.Visibility = this.Visibility - 50;
+            imageMode(CENTER);
+            image(this.image, pos.x,pos.y,50, 50);
+            pop(); 
+        }
+        else{
+         var pos= this.body.position;
+          var angle = this.body.angle;
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          imageMode(CENTER);
+          image(this.image, 0, 0, this.width, this.height);
+          pop();
 
-        imageMode(CENTER);
-        image(this.image, pos.x, pos.y, 50, 50);
-    
-    }
-}
+        }
+        
+        
+      }
+  }
